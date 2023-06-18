96586fda9cedc96ce09333a9b504abd9adb1cc78
Shoichi <funyu19930711@gmail.com>
2023-06-18 12:23:52 +0900

    上流ブランチ？リモートブランチ？のfeatures/01の内容を反映

--- eLearningDynamicV1/myapp/source_exec.py
+++ eLearningDynamicV1/myapp/source_exec.py
@@ -2,19 +2,30 @@ import bs4 #ライブラリbs4をインポートする
 from jinja2 import Template
 import string
 import yaml
+import re
+
+def convert_list_elements(my_list, data):
+    # converted_list = []
+    # for item in my_list:
+        # 正規表現を使用してマスタッシュ記法の変数を抽出し、対応する値で置換する
+    # converted_list = re.sub(r"\{\{(\w+)\}\}", lambda match: data.get(match.group(1), match.group()), my_list)
+    converted_list = re.sub(r"\|(\w+)\|", lambda match: data.get(match.group(1), match.group()), my_list)
+    # converted_list.append(converted_item)
+    return converted_list
 
 #bs4で定義された関数を使ってsample.htmlを読み取る
-html = open('../templates/myapp/learn_aws_tests_one_template.html')
+html = open('../templates/myapp/learn_git_tests_one_template.html')
 html_read = html.read()
-print(html_read)
 template = Template(html_read)
-with open('../templates/myapp/learn_aws_tests.yaml') as file:
+with open('../templates/myapp/learn_git_tests.yaml') as file:
     obj = yaml.safe_load(file)
 # print(obj['array'][0])
 base_data = {}
 for num in range(len(obj['array'])):
     print("num:"+str(num))
     for k, v in obj['array'][num].items():
+        e = num+1
+        print(k+str(e))
         data = {
         k+str(num) : v,
         # 's1-'+str(num) : question['s1'],
@@ -28,8 +39,10 @@ for num in range(len(obj['array'])):
         base_data = {**base_data, **data}
 print(base_data)
 out = template.render(base_data)
-# print(template)
-with open("../templates/myapp/test.html", "w") as f:
-    f.write(out)
+print(out)
+results = convert_list_elements(out, base_data)
+print(results)
+with open("../templates/myapp/learn_git_tests_one.html", "w") as f:
+    f.write(results)
 
 print("書き込みが完了しました")
