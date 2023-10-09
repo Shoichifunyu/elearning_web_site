8a90d965e6facc3be9439aef60f2d2e853b608c8
Shoichi <funyu19930711@gmail.com>
2023-10-09 17:19:52 +0900

    プログラムの関数化

--- eLearningDynamicV1/myapp/contents_source_exec.py
+++ eLearningDynamicV1/myapp/contents_source_exec.py
@@ -3,39 +3,49 @@ import string
 import yaml
 import re
 
-def convert_list_elements(my_list, data):
-    converted_list = re.sub(r"\|(\w+)\|", lambda match: data.get(match.group(1), match.group()), my_list)
-    # converted_list.append(converted_item)
+def prepare_generate_html(html_template, yaml_contents):
+    open_template = open(html_template)
+    read_template = open_template.read()
+    process_template = Template(read_template)
+
+    with open(yaml_contents) as open_yaml:
+        load_yaml = yaml.safe_load(open_yaml)
+
+    return process_template, load_yaml
+
+def generate_html_contents(load_yaml, process_template):        
+    collect_data = {}
+    for section_cnt in range(len(load_yaml['array'])):
+        print("num:"+str(section_cnt))
+        for title, description in load_yaml['array'][section_cnt].items():
+            section_num = section_cnt + 1
+            print(title+str(section_num))
+            section_data = {
+            title+str(section_num) : description,
+            }
+            collect_data = {**collect_data, **section_data}
+
+    print(collect_data)
+    render_template = process_template.render(collect_data)
+    print(render_template)
+    generated_html_contents = convert_list_elements(render_template, collect_data)
+    print(generated_html_contents)
+    return generated_html_contents
+
+def convert_list_elements(list, data):
+    converted_list = re.sub(r"\|(\w+)\|", lambda match: data.get(match.group(1), match.group()), list)
     return converted_list
 
-html_template = '../templates/myapp/learn_react_contents_one_template.html'
-yaml_contents = '../templates/myapp/learn_react_contents_one.yaml'
-html_product = '../templates/myapp/learn_react_contents_one.html'
+def make_html_file(generated_html_contents, html_product):
+    with open(html_product, "w") as f:
+        f.write(generated_html_contents)
+    print("書き込みが完了しました")
 
-open_template = open(html_template)
-read_template = open_template.read()
-process_template = Template(read_template)
+if __name__ == '__main__':
+    html_template = '../templates/myapp/learn_react_contents_one_template.html'
+    yaml_contents = '../templates/myapp/learn_react_contents_one.yaml'
+    html_product = '../templates/myapp/learn_react_contents_one.html'
 
-with open(yaml_contents) as open_yaml:
-    load_yaml = yaml.safe_load(open_yaml)
-    
-collect_data = {}
-for section_cnt in range(len(load_yaml['array'])):
-    print("num:"+str(section_cnt))
-    for title, description in load_yaml['array'][section_cnt].items():
-        section_num = section_cnt + 1
-        print(title+str(section_num))
-        section_data = {
-        title+str(section_num) : description,
-        }
-        collect_data = {**collect_data, **section_data}
-
-print(collect_data)
-render_template = process_template.render(collect_data)
-print(render_template)
-results = convert_list_elements(render_template, collect_data)
-print(results)
-with open(html_product, "w") as f:
-    f.write(results)
-
-print("書き込みが完了しました")
+    process_template, load_yaml = prepare_generate_html(html_template, yaml_contents)
+    generated_html_contents = generate_html_contents(load_yaml, process_template)
+    make_html_file(generated_html_contents, html_product)
